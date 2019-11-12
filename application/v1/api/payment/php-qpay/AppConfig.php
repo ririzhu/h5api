82cdf1b55ef51934ff21d815ec1be6d0b26edd6b
sail <sail@horizou.com>
2019-11-12 09:16:06 +0800

    支付回调

---
+++ application/v1/api/payment/php-qpay/AppConfig.php
@@ -0,0 +1,14 @@
+﻿<?php
+/**
+ * 常量配置
+ */
+ class AppConfig{
+//	const APPID = '00000156';
+	const APPID = '00178859';
+//	const CUSID = '990581053996001';
+	const CUSID = '5505810078000YD';
+	const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
+    const APIURL = "http://test.allinpaygd.com/apiweb/qpay";//测试环境
+    const APIVERSION = '11';
+ }
+?>
---
+++ application/v1/controller/Notify.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\v1\controller;
+
+use app\v1\model\Payment;
+
+
+/**
+ * 回调
+ *
+ */
+
+class Notify extends Base
+{
+	public function allinpayNotify()
+    {
+        $params = array();
+        foreach($_POST as $key=>$val) {
+            $params[$key] = $val;
+        }
+        if (count($params) < 1){
+            echo 'error';
+            exit();
+        }
+        if ($params['retcode'] != 'SUCCESS'){
+            echo 'error';
+            exit();
+        }
+        if (!\AppUtil::ValidSign($params,\AppConfig::APPKEY)){
+            echo 'Sign 错误';
+            exit();
+        }
+        if (empty($params['orderid'])){
+            echo 'error';
+            exit();
+        }
+        $payment = new Payment();
+        $process = $payment->notifyProcess($params['orderid']);
+        if ($process['code'] == 200){
+            echo 'success';
+        }else{
+            echo 'error';
+        }
+	}
+
+}
